1ea6dcebbda5e644e285ecb5b27aad2ffd66a2f9
ranjit12412 <Ranjit@Ranjit-PC>
2018-04-29 02:38:58 +0530

    Change ny ranjit for class 2

---
+++ src/testPackege/ClassB.java
@@ -0,0 +1,12 @@
+package testPackege;
+
+public class ClassB {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("My name is Ranjit");
+
+	}
+
+}
