24064a5461e83c33c125826b7b5e741d06a3a08f
ranjit12412 <Ranjit@Ranjit-PC>
2018-04-29 02:08:42 +0530

    QWDWED

---
+++ src/testPackege/ClassDemo.java
@@ -0,0 +1,11 @@
+package testPackege;
+
+public class ClassDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hi how are you?");
+	}
+
+}
